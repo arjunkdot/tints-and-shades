ebbfa7e5f08c088f24c555ef7c8a67210062ffac
arjunkdot <78548960+arjunkdot@users.noreply.github.com>
2023-10-10 20:50:52 +0530

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+function App() {
+  return <div className="text-2xl bg-blue-100 text-blue-600 w-full h-[100vh] flex items-center justify-center">Look ma, no hands 👐</div>;
+}
+
+export default App;
